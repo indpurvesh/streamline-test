5d6d492c3e3bb7752490f2a8ea34e35d72c7aac9
Purvesh <ind.purvesh@gmail.com>
2020-10-14 15:44:16 +1300

    csrf token added

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -12,12 +12,13 @@
   <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
     <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
       <form action="{{ route('login') }}" method="POST">
+        @csrf
         <div>
           <label for="email" class="block text-sm font-medium leading-5 text-gray-700">
             Email address
           </label>
           <div class="mt-1 rounded-md shadow-sm">
-            <input id="email" type="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+            <input name="email" id="email" type="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
           </div>
         </div>
 
@@ -26,7 +27,7 @@
             Password
           </label>
           <div class="mt-1 rounded-md shadow-sm">
-            <input id="password" type="password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+            <input name="password" id="password" type="password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
           </div>
         </div>
 
